9cb7aca8496d7587250c216eabc41b9bd294e3ff
deepakjanorkar <48157935+deepakjanorkar@users.noreply.github.com>
2020-10-01 18:15:40 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,2 +1,6 @@
-#program for finding factorial of a number
-Factorial = lambda n: reduce(lambda x, y: x * y, range(1, n+1)) 
+# Program to filter out only the even items from a list
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+
+print(new_list)
